c0a208d33de7951f663c8b0a32df54546f9b3785
eda84 <gerorin1010@gmail.com>
2017-08-19 22:30:00 +0900

    preloadを追加

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ let game    = new Phaser.Game(640, 480, Phaser.AUTO, "");
 // MainStateクラスを、"mainState"という名前で状態登録
 game.state.add("mainState", MainState);
 
+function preload() {
+    game.load.image('star', 'assets/images.jpg')
+}
+
 window.onload   = () => {
     // 登録した状態のうち、"mainState"をスタートする
     game.state.start("mainState");
+    game.add.sprite(0,0,'star')
 }
